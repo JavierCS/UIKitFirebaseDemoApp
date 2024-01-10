f1874b19ecf7eb05910d7d0d0582c88db2b5e10d
Javier Cruz Santiago <javiercruzsantiago@icloud.com>
2024-01-09 22:46:05 -0600

    [NEW]
    Xcode project creation

---
+++ UIKitFirebaseDemoApp/UIKitFirebaseDemoApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIKitFirebaseDemoApp
+//
+//  Created by Javier Cruz Santiago on 09/01/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
